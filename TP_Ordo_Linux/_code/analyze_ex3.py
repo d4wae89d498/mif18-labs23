6fbc1b738fa2033b13ff3dd071a15c1e59b90a40
ddwadwa <faussurier.marc@icloud.com>
2023-11-06 11:47:08 +0100

    python code

--- TP_Ordo_Linux/_code/analyze_ex3.py
+++ TP_Ordo_Linux/_code/analyze_ex3.py
@@ -4,7 +4,7 @@
 
 """
 MIF18 TP Linux scheduling
-Script pour tracer les courbes de l'exo 3. 
+Script pour tracer les courbes de l'exo 3.
 V1 Grégoire Pichon et Laure Gonnord, Avril 2020
 usage python3 test_ex3.py
 """
@@ -34,7 +34,7 @@ def acquire_results(nbp):
         ## Lancer les programmes en tâche de fond
         os.system("taskset -c 0 nice -n 0          ./ex3.exe"+ "> /dev/null &")
         os.system("taskset -c 0 nice -n "+str(i)+" ./ex3.exe "+"output_"+str(i)+".csv > /dev/null &")
-        
+
         # Attendre puis les tuer
         os.system("sleep 20")
         os.system("pkill -INT ex3.exe")
@@ -44,19 +44,34 @@ def acquire_results(nbp):
 
 
 def parse_results(nbp):
-    # SD pour stocker 
     X=np.zeros([nbp, 1])
     Y1=np.zeros([nbp, 1])
     Y2=np.zeros([nbp, 1])
-
     for i in range (0, nbp):
-        # Parser les resultats: on recupere les 20 premières lignes (sauf la premiere), au plus.
-        # à la fin X[i] <-i, Y1[i] <- moyenne de waiting et Y2[i] <- moyenne elapsed
-        # HINT : utiliser csv.DictReader
+        waiting_times = []
+        elapsed_times = []
+        try:
+            with open(f"output_{i}.csv", 'r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row_num, row in enumerate(reader):
+                    if row_num >= 20:
+                        break
+                    waiting_times.append(float(row['waiting']))
+                    elapsed_times.append(float(row['elapsed']))
+        except FileNotFoundError:
+            print(f"File output_{i}.csv not found. Skipping.")
+            continue
+        if not waiting_times or not elapsed_times:
+            print(f"No data in output_{i}.csv or data is corrupt.")
+            continue
+        X[i] = i
+        Y1[i] = sum(waiting_times) / len(waiting_times) if waiting_times else 0
+        Y2[i] = sum(elapsed_times) / len(elapsed_times) if elapsed_times else 0
     return (X, Y1, Y2)
 
+
 def plot(X, Y1, Y2):
-    
+
 # plot config
     font = {'family' : 'normal',
             'weight' : 'bold',
